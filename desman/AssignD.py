7fbe80c7da91c0d63c0d5b6be2a08b6971075c34
Christopher Quince <u1375038@004851.home>
2015-08-06 18:07:36 +0100

    Fast assignment

--- desman/AssignD.py
+++ desman/AssignD.py
@@ -32,18 +32,21 @@ def main(argv):
 
     parser.add_argument("assign_file", help="assign file")
 
+    parser.add_argument('--fast', dest='fast', action='store_true')
+
     parser.add_argument('-o','--output_dir', type=str, default="output",
         help=("string specifying output directory and file stubs"))
 
     #get command line arguments  
     args = parser.parse_args()
     
-    import ipdb; ipdb.set_trace()
+#    import ipdb; ipdb.set_trace()
     
     gamma_file = args.gamma_file
     eta_file = args.eta_file
     assign_file = args.assign_file
     output_dir = args.output_dir
+    fast = args.fast
     
     variants    = p.read_csv(assign_file, header=0, index_col=0)
     variants_matrix = variants.as_matrix()
@@ -76,8 +79,12 @@ def main(argv):
         assigns    = p.read_csv(assign_file, header=0, index_col=0)    
         assigns_matrix = assigns.as_matrix()
         assigns_matrix = np.delete(assigns_matrix, 0, 1)
-     #  import ipdb; ipdb.set_trace()
-        (assignTau,confTau) = haplo_SNP.assignTau(assigns_matrix)
+        
+        if fast is True:
+            assignTau = haplo_SNP.assignTauFast(assigns_matrix)
+        else:
+            (assignTau,confTau) = haplo_SNP.assignTau(assigns_matrix)
+        
         snda = haplo_SNP.calculateSND(assignTau)
         
         assign_contig_names = assigns.index.tolist()
@@ -86,22 +93,24 @@ def main(argv):
         AV = assigns_matrix.shape[0]
         assign_tau_res = np.reshape(assignTau,(AV,haplo_SNP.G*4))
         assign_tau_df = p.DataFrame(assign_tau_res,index=assign_contig_names)
-        conf_tau_df = p.DataFrame(confTau,index=assign_contig_names)
-        
         assign_tau_df['Position'] = assign_position
-        conf_tau_df['Position'] = assign_position
-        
         cols = assign_tau_df.columns.tolist()
         cols = cols[-1:] + cols[:-1]
         
         assign_tau_df = assign_tau_df[cols]
         assign_tau_df.to_csv(output_dir+"/Assigned_Tau_star.csv")
-    
-        cols = conf_tau_df.columns.tolist()
-        cols = cols[-1:] + cols[:-1]
         
-        conf_tau_df = conf_tau_df[cols]
-        conf_tau_df.to_csv(output_dir+"/Assigned_Tau_conf.csv")
+        if fast is False:
+        
+            conf_tau_df = p.DataFrame(confTau,index=assign_contig_names)
+            
+            conf_tau_df['Position'] = assign_position
+        
+            cols = conf_tau_df.columns.tolist()
+            cols = cols[-1:] + cols[:-1]
+        
+            conf_tau_df = conf_tau_df[cols]
+            conf_tau_df.to_csv(output_dir+"/Assigned_Tau_conf.csv")
     
 if __name__ == "__main__":
     main(sys.argv[1:])
