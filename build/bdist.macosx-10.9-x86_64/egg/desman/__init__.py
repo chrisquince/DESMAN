2c35941ca021bdfe18bb4bdaff2d4e653a2e7525
Christopher Quince <u1375038@004851.home>
2016-03-03 17:50:52 +0000

    Does this suffering never end?

---
+++ build/bdist.macosx-10.9-x86_64/egg/desman/__init__.py
@@ -0,0 +1,2 @@
+import pkg_resources  # part of setuptools
+__version__ = pkg_resources.require("desman")[0].version
--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 import sys, os
-from distutils.core import Extension
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
 
 version = '0.1dev'
 
-module1 = Extension('vbgmm',
-        libraries =['gsl',  'gslcblas'],
-        include_dirs = ['c-concoct'],
-        sources = ['c-concoct/vbgmmmodule.c'])
+
+
+module1 = Extension("sampletau",
+                             sources=["sampletau/sampletau.pyx", "sampletau/c_sample_tau.c"],
+                             extra_compile_args=['-O3', '-march=native'],
+                             libraries =['gsl',  'gslcblas'],
+                             include_dirs=[numpy.get_include(), '/opt/local/include/'])
 
 setup(name='desman',
       version=version,
       description="De novo Extraction of Strains from MetAgeNomes",
       long_description="""DESMAN is a pipeline that facilities the
       completely de novo extraction of both strain haplotypes
-      and accessory genomes de novo from metagenome data"""
+      and accessory genomes de novo from metagenome data""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='Metagenomics Python',
       author='Christopher Quince',
@@ -24,10 +29,11 @@ setup(name='desman',
       maintainer_email='c.quince@warwick.ac.uk',
       url='https://github.com/chrisquince/DESMAN',
       license='FreeBSD',
-      packages=['desman']),
+      packages=['desman'],
       scripts=["bin/desman"],
       include_package_data=True,
       zip_safe=False,
+      cmdclass = {'build_ext': build_ext},
       ext_modules=[module1],
       install_requires=['cython>=0.19.1',
                         'numpy>=1.7.1',
