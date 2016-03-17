64017b59a6b565fa458c67df4099a417887216e2
Christopher Quince <u1375038@004851.home>
2016-03-17 17:32:56 +0000

    Calc lengths script added

---
+++ scripts/Lengths.py
@@ -0,0 +1,20 @@
+from Bio import SeqIO
+
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-i", "--inputfile", dest="ifilename",
+                  help="fasta file", metavar="FILE")
+
+
+
+(options, args) = parser.parse_args()
+
+minqual = 20
+
+handle = open(options.ifilename, "rU")
+for record in SeqIO.parse(handle, "fasta"):
+	seq = record.seq
+	
+	print record.id + " " + repr(len(seq))
+handle.close()
