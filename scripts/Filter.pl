2a11affe9490975109687f107eca4ca52ddc1e6b
chrisquince <c.quince@warwick.ac.uk>
2016-07-15 13:10:51 +0100

    Added files

---
+++ scripts/Filter.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+my $token = $ARGV[0];
+my $min = 0.5;
+
+while($line = <STDIN>){
+    chomp($line);
+
+    my @tokens = split(/,/,$line);
+
+    $tok = $tokens[$token];
+
+    if($tok =~/(.*)->(.*)->(.*)/){
+        if ($2 > $min && $3 > $min){
+            print "$tokens[0],$1\n";
+        }
+    }
+}
