e7b09a681a9657612a7a7ebb93623623a60a8b93
johnne <john.sundh@scilifelab.se>
2018-04-12 15:26:19 +0200

    Added extra compile args in case someone is installing with conda compilers

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 from setuptools import setup
+import sysconfig
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 from glob import glob
-
+import os
 version = '0.1dev'
 
-
+# Since Conda started using their own compilers the include dir may not be found when trying to install within an
+# active conda environment.
+# Here we explicitly set the include path for the current environment.
+extra_include_path = os.path.join(sysconfig.get_path("data"),"include")
+extra_compile_args = "-I{}".format(extra_include_path)
 
 module1 = Extension("sampletau",
                              sources=["sampletau/sampletau.pyx", "sampletau/c_sample_tau.c"],
-                             extra_compile_args=['-O3', '-march=native'],
-                             libraries =['gsl',  'gslcblas'],
+                             extra_compile_args=['-O3', '-march=native', extra_compile_args],
+                                 libraries =['gsl',  'gslcblas'],
                              include_dirs=[numpy.get_include(), '/opt/local/include/'])
 
 setup(name='desman',
@@ -30,11 +35,12 @@ setup(name='desman',
       url='https://github.com/chrisquince/DESMAN',
       license='FreeBSD',
       packages=['desman'],
-      scripts=["bin/desman","desman/Variant_Filter.py"]+glob("scripts/*"),
+      scripts=["bin/desman","desman/Variant_Filter.py", "desman/GeneAssign.py"]+glob("scripts/*"),
       include_package_data=True,
       zip_safe=False,
-      cmdclass = {'build_ext': build_ext},
+      cmdclass={'build_ext': build_ext},
       ext_modules=[module1],
+      extra_compile_args=extra_compile_args,
       install_requires=['cython>=0.19.1',
                         'numpy>=1.7.1',
                         'scipy>=0.12.0',
